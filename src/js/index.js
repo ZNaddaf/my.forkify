02e97ea06fddc49654e9f2d5249b18a1e8ad7ec8
N0matic <z.naddaf@protonmail.com>
2020-07-24 12:25:06 -0500

    search result pagination implemented

--- src/js/index.js
+++ src/js/index.js
@@ -41,6 +41,14 @@ const conrolSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     conrolSearch();
-})
+});
 
-search.getResults();
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        console.log(goToPage);
+    }
+});
